8a56de0decb9d170c6b304aebef4c91f01964c88
Petros Zavrakas <zav.petros@gmail.com>
2025-04-02 15:57:49 +0200

    feat: remove node_module and add gitignore

--- node_modules/@nx/expo/src/executors/install/schema.d.ts
+++
@@ -1,8 +0,0 @@
-// options from https://github.com/expo/expo/blob/main/packages/@expo/cli/src/install/index.ts
-
-export interface ExpoInstallOptions {
-  packages?: string | string[]; // either a string separated by comma or a string array
-  check?: boolean; // default is false
-  fix?: boolean; // default is false
-  force?: boolean; // default is false
-}
--- node_modules/@nx/expo/src/executors/serve/schema.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ExpoServeExecutorSchema {
-  port: number;
-  dev?: boolean;
-  minify?: boolean;
-  https?: boolean;
-  clear?: boolean;
-}
